ed4637a7885ae3c60ce463bb7bed4ef184c40044
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2024-07-12 09:30:13 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -29,3 +29,58 @@ function submitForm() {
         }
     });
 }
+
+    const canvas = document.getElementById('fireCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const fireParticles = [];
+const numberOfParticles = 100;
+
+class FireParticle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 2;
+        this.speedX = (Math.random() * 3 - 1.5) * 2;
+        this.speedY = (Math.random() * 3 - 1.5) * 2;
+        this.color = `rgba(275, ${Math.random() * 255}, 0.6, 0.7)`;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function init() {
+    for (let i = 0; i < numberOfParticles; i++) {
+        fireParticles.push(new FireParticle());
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < fireParticles.length; i++) {
+        fireParticles[i].update();
+        fireParticles[i].draw();
+        if (fireParticles[i].size <= 0.2) {
+            fireParticles.splice(i, 1);
+            fireParticles.push(new FireParticle());
+        }
+    }
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
+
